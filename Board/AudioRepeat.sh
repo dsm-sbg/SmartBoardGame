d1a4b5e9247f381d3b55bfcd692acff448f3a204
hanugogi <hyinwoojjang@gmail.com>
2019-06-21 23:54:44 +0900

    Finish Background

--- Board/AudioRepeat.sh
+++ Board/AudioRepeat.sh
@@ -4,6 +4,6 @@ if (($# < 1)); then
 	echo "$0 [Audio File]"
 else
 	while ((1)); do
-		mpg123 $1
+		mpg123 -q $1
 	done
 fi
--- Board/BG.py
+++ Board/BG.py
@@ -1,101 +1,95 @@
 import os
+import sys
 import time
 import pygame
-import signal
 
-pid = os.fork()
+flag_FULL = False; pre_flag_FULL = False
+flag_CONN = False; pre_flag_FULL = False
+flag_TURN = 1; pre_flag_FULL = 1
 
-if (pid == 0):
-    os.system("sudo ./AudioRepeat.sh Egypt_Theme.mp3")
+backgroundSize = (1280, 720)
 
-else:
-    bgSize = (1280, 720)
+gameDisplay = pygame.display.set_mode(backgroundSize, pygame.DOUBLEBUF)
+pygame.display.set_caption('SMART BOARD GAME')
 
-    flag_FULL = 0; pre_flag_FULL = 0
-    flag_CONN = 0; pre_flag_FULL = 0
-    flag_TURN = 1; pre_flag_FULL = 1
+Door_Path = "/home/pi/SmartBoardGame/Board/Source/Open_Door/"
+Door_Squares = [587, 708, 503, 362, 849, 981, 230, 107]
 
+Door_Floors = [90, 223, 223, 356, 356, 489, 489, 622]
+
+def InitBackground():
     pygame.init()
 
-    gameDisplay = pygame.display.set_mode(bgSize, pygame.DOUBLEBUF)
-    pygame.display.set_caption('SMART BOARD GAME')
+    DrawBackground()
+    time.sleep(1)
 
-    crashed = False
+    DrawDoor()
+    time.sleep(1)
 
-    door_path = "./Source/Door_Open/"
+def Changed():
+    res = 0
 
-    def Changed():
-        res = 0
+    global flag_FULL
+    global flag_CONN
+    global flag_TURN
 
-        global flag_FULL
-        global flag_CONN
-        global flag_TURN
+    global pre_flag_FULL
+    global pre_flag_CONN
+    global pre_flag_TURN
 
-        global pre_flag_FULL
-        global pre_flag_CONN
-        global pre_flag_TURN
+    if pre_flag_FULL != flag_FULL :
+        res = 1
+    elif pre_flag_CONN != flag_CONN :
+        res = 1
+    elif pre_flag_TURN != flag_TURN :
+        res = 1
 
-        if pre_flag_FULL != flag_FULL :
-            res = 1
-        elif pre_flag_CONN != flag_CONN :
-            res = 1
-        elif pre_flag_TURN != flag_TURN :
-            res = 1
+    pre_flag_FULL = flag_FULL
+    pre_flag_CONN = flag_CONN
+    pre_flag_TURN = flag_TURN
 
-        pre_flag_FULL = flag_FULL
-        pre_flag_CONN = flag_CONN
-        pre_flag_TURN = flag_TURN
+    return res
 
-        return res
+def DrawDoor():
+    for i in range(0, 8):
+        gameDisplay.blit(pygame.image.load(Door_Path + "Open_Door1.png"), (Door_Squares[i], Door_Floors[i]))
 
-    def Gate():
-            gameDisplay.blit(pygame.image.load(door_path + "Door_Open1.png"),(587,90))
-            gameDisplay.blit(pygame.image.load(door_path + "Door_Open1.png"),(503,223))
-            gameDisplay.blit(pygame.image.load(door_path + "Door_Open1.png"),(708,223))
-            gameDisplay.blit(pygame.image.load(door_path + "Door_Open1.png"),(362,356))
-            gameDisplay.blit(pygame.image.load(door_path + "Door_Open1.png"),(849,356))
-            gameDisplay.blit(pygame.image.load(door_path + "Door_Open1.png"),(230,489))
-            gameDisplay.blit(pygame.image.load(door_path + "Door_Open1.png"),(981,489))
-            gameDisplay.blit(pygame.image.load(door_path + "Door_Open1.png"),(107,622))
+def DrawBackground():
+    gameDisplay.blit(pygame.image.load("./BG.png"),(0,0))
 
-            if not flag_CONN:
-                gameDisplay.blit(pygame.image.load("./Connect Failed.png"), (30, 5))
+    if not flag_CONN:
+        gameDisplay.blit(pygame.image.load("./Connect Failed.png"), (30, 5))
 
-            if(Changed()):
-                pygame.display.update()
+def OpenDoor(num):
+    os.system("mpg123 -q BGM/Close_Door.mp3 &")
 
-
-    def Draw_image():
-        gameDisplay.blit(pygame.image.load("./BG.png"),(0,0))
-
-    while not crashed:
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                crashed = True
-
-            if event.type is pygame.KEYDOWN and event.key == pygame.K_f:
-                if flag_FULL == 1:
-                    gameDisplay = pygame.display.set_mode((0, 0), pygame.FULLSCREEN | pygame.DOUBLEBUF)
-                    flag_FULL = 0
-                else:
-                    gameDisplay = pygame.display.set_mode(bgSize, pygame.DOUBLEBUF)
-                    flag_FULL = 1
-
-            if event.type is pygame.KEYDOWN and event.key == pygame.K_c:
-                flag_CONN = 0
-            elif event.type is pygame.KEYUP and event.key == pygame.K_c:
-                flag_CONN = 1
-
-            if event.type is pygame.KEYDOWN and event.key == pygame.K_q:
-                os.system("sudo killall AudioRepeat.sh")
-                os.system("sudo killall mpg123")
-                os.system("sudo killall python3")
-
-            print(event)
-
-        Draw_image()
-        Gate()
+    for i in range(1, 8):
+        gameDisplay.blit(pygame.image.load(Door_Path + "Open_Door"+ str(i)  +".png"),(Door_Squares[num], Door_Floors[num]))
+        time.sleep(0.1)
         pygame.display.update()
 
-    pygame.quit()
+def CloseDoor(num):
+    os.system("mpg123 -q BGM/Close_Door.mp3 &")
+    for i in range(7, 0, -1):
+        gameDisplay.blit(pygame.image.load(Door_Path + "Open_Door"+ str(i)  +".png"),(Door_Squares[num], Door_Floors[num]))
+        time.sleep(0.1)
+        pygame.display.update()
+
+def ChangeFULL():
+    global flag_FULL
+
+    if flag_FULL == 1:
+        gameDisplay = pygame.display.set_mode((0, 0), pygame.FULLSCREEN | pygame.DOUBLEBUF)
+        flag_FULL = 0
+    else:
+        gameDisplay = pygame.display.set_mode(backgroundSize, pygame.DOUBLEBUF)
+        flag_FULL = 1
+
+
+def ChangeCONN(status):
+    global flag_CONN
+
+    if status == True:
+        flag_CONN = 1
+    else:
+        flag_CONN = 0
